0e4e3609b5cc22f32fdf4410c44f908936e3c6a2
Sandra-Flores <sfloressilverio@csumb.edu>
2020-05-07 15:34:33 -0700

    Fix: Added default values.

--- src/main/resources/db/migration/V1__table_schemas.sql
+++ src/main/resources/db/migration/V1__table_schemas.sql
@@ -7,16 +7,16 @@ CREATE TABLE product(
 CREATE TABLE review(
     id INT AUTO_INCREMENT primary key,
     title VARCHAR(255) NOT NULL,
-    is_top_review BIT NULL,
+    is_top_review TINYINT NULL default 0,
     time_created  timestamp default CURRENT_TIMESTAMP NULL,
-    review_txt VARCHAR(100) NULL,
+    review_txt VARCHAR(100) NULL default '',
     product_id  INT NOT NULL
 );
 
 CREATE TABLE comment(
     id INT AUTO_INCREMENT primary key,
     title VARCHAR(255) NOT NULL,
-    comment_txt VARCHAR(100) NOT NULL,
+    comment_txt VARCHAR(100) NULL default '',
     time_created  timestamp default CURRENT_TIMESTAMP NULL,
     review_id  INT NOT NULL
 );

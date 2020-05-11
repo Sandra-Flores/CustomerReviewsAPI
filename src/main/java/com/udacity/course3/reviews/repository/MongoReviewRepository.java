d62814bf68bd5f5283cf3d83d21e2c07dd65609c
Sandra-Flores <sfloressilverio@csumb.edu>
2020-05-10 23:48:21 -0700

    Chore: Clean up imports.

--- src/main/java/com/udacity/course3/reviews/repository/MongoReviewRepository.java
+++ src/main/java/com/udacity/course3/reviews/repository/MongoReviewRepository.java
@@ -2,9 +2,8 @@ package com.udacity.course3.reviews.repository;
 
 import com.udacity.course3.reviews.entity.MongoReview;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface MongoReviewRepository extends MongoRepository<MongoReview, Integer> {
 
 }
